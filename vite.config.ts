3c9006fd315427cacd88ab4d132c0b0ca8c9c67f
fwc <1643806656@qq.com>
2023-09-09 00:26:35 +0800

    chore: update the project template

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), UnoCSS()],
+  plugins: [UnoCSS(), react()],
   resolve: {
     alias: {
       '@': '/src'
